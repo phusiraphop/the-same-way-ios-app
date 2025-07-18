9510630b9e223f3cef75f9ab01ca5e8c807e2be6
Siraphop Chaisirikul <pooh_siraphop@hotmail.com>
2025-07-18 19:43:20 +0700

    WIP: Create new file

--- TheSameWay/TheSameWay/ContentView.swift
+++ TheSameWay/TheSameWay/ContentView.swift
@@ -17,29 +17,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-
-import GoogleMaps
-
-struct GoogleMapView: UIViewRepresentable {
-    func makeUIView(context: Context) -> GMSMapView {
-        let camera = GMSCameraPosition(latitude: 13.7563, longitude: 100.5018, zoom: 14.0)
-        let options = GMSMapViewOptions()
-        options.camera = camera
-
-        let mapView = GMSMapView(options: options)
-
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 13.7563, longitude: 100.5018)
-        marker.title = "กรุงเทพฯ"
-        marker.snippet = "ประเทศไทย"
-        marker.map = mapView
-
-        return mapView
-    }
-
-    func updateUIView(_ uiView: GMSMapView, context: Context) {
-        // อัปเดตแผนที่ถ้าต้องการในอนาคต
-    }
-}
-
---
+++ TheSameWay/TheSameWay/GoogleMapView.swift
@@ -0,0 +1,37 @@
+//
+//  GoogleMapView.swift
+//  TheSameWay
+//
+//  Created by Siraphop Chaisirikul on 18/7/2568 BE.
+//
+
+import SwiftUI
+
+
+import GoogleMaps
+
+struct GoogleMapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> GMSMapView {
+        let camera = GMSCameraPosition(latitude: 13.7563, longitude: 100.5018, zoom: 14.0)
+        let options = GMSMapViewOptions()
+        options.camera = camera
+
+        let mapView = GMSMapView(options: options)
+
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 13.7563, longitude: 100.5018)
+        marker.title = "กรุงเทพฯ"
+        marker.snippet = "ประเทศไทย"
+        marker.map = mapView
+
+        return mapView
+    }
+
+    func updateUIView(_ uiView: GMSMapView, context: Context) {
+        // อัปเดตแผนที่ถ้าต้องการในอนาคต
+    }
+}
+
+#Preview {
+    GoogleMapView()
+}
